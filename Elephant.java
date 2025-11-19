79ab3ef564dd5a1d4bbdf04e258a5748b7240f87
adelinalyw <adelina.lyw@gmail.com>
2025-11-19 13:02:54 -0500

    added apple and elephant

---
+++ Elephant.java
@@ -0,0 +1,26 @@
+import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
+
+/**
+ * the Elephant, our hero.
+ * 
+ * @author (Adelina) 
+ * @version (11, 19, 2025)
+ */
+public class Elephant extends Actor
+{
+    /**
+     * Act - do whatever the Elephant wants to do. This method is called whenever
+     * the 'Act' or 'Run' button gets pressed in the environment.
+     */
+    public void act()
+    {
+        // Add your action code here.
+        if (Greenfoot.isKeyDown("left") ) {
+            move(-1);
+        } else if (Greenfoot.isKeyDown("right") ) {
+            move(1);
+        }
+        
+        removeTouching (Apple.class);
+    }
+}
--- MyWorld.java
+++ MyWorld.java
@@ -3,5 +3,20 @@ import greenfoot.*;
 public class MyWorld extends World {
     public MyWorld() {
         super(600, 400, 1);
+        
+        Elephant elephant = new Elephant();
+        addObject(elephant, 300, 200);
+        
+        createApple();
+    }
+    
+    /**
+     * Create a new apple at random location at top of screen
+     */
+    public void createApple() {
+        Apple apple = new Apple();
+        int x = Greenfoot.getRandomNumber (600);
+        int y = 0;
+        addObject(apple, x, y);
     }
 }
